97e951210595028edbc7c73e8ad5ecae0cd102c0
Kristiyan Ivanov <78723791+krsivanov98@users.noreply.github.com>
2023-01-11 17:05:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function getRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
     for (let i=0; i<6; i++){
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16 + 1)];
     }
     return color;
 }
